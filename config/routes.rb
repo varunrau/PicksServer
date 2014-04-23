4573fefc62bff52e4b2e99c5c10a2221bfb0b580
varunrau <varunrau@gmail.com>
2014-04-23 01:39:53 -0700

    added upload route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Picks::Application.routes.draw do
   scope path: :api, defaults: {format: :json} do
     scope path: :v1 do
       resource :user
+      get ':facebook_id/upload/', to: 'user#upload', as: :upload
     end
   end
 end
