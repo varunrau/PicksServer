222fc1dc336df2e67346a766e75c5785cc815d6b
varunrau <varunrau@gmail.com>
2014-04-24 13:28:36 -0700

    picture beauty score

--- app/controllers/api/v1/picture_controller.rb
+++ app/controllers/api/v1/picture_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::PictureController < ApplicationController
   def rate
     user = User.find params[:facebook_id]
     picture = Picture.find image: params[:uuid]
+    picture.num_ratings = picture.num_ratings + 1
+    picture.num_liked = picture.num_liked + params[:liked]
+    picture.beauty_score = picture.num_liked/picture.num_ratings
+    picture.save
     Views.create facebook_id: user.facebook_id, picture_id: picture.image
   end
 end
