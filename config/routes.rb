91ee2115d83e86dd9012709a7d8c64f552ab1148
varunrau <varunrau@gmail.com>
2014-04-24 16:22:56 -0700

    added comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Picks::Application.routes.draw do
   get "picture/rate"
   scope path: :api, defaults: {format: :json} do
     scope path: :v1 do
+      # create new user
       post 'new', controller: 'api/v1/user'
+      # upload picture
       post 'upload', controller: 'api/v1/user'
 
+      # Get all pictures -- don't use
       get 'pictures', controller: 'api/v1/pictures'
+      # A user rates an picture
       post 'rate', controller: 'api/v1/pictures'
     end
   end
