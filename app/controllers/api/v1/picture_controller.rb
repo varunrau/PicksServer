08b1ee92ce4b2f8352be6f1e9d0f921ddcfe2142
varunrau <varunrau@gmail.com>
2014-04-23 23:25:30 -0700

    return relevant picks

--- app/controllers/api/v1/picture_controller.rb
+++ app/controllers/api/v1/picture_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::PictureController < ApplicationController
   def index
+    pictures = Picture.get_pictures
+    render json: { message: "Pictures", pictures: Picture.collection_to_json(pictures) }
   end
 
   def rate
